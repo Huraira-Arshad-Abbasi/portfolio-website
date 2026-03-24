764fbb3297b314ed4174e56f4ea2bef97601d38f
Huraira Arshad <hurairaabbasi647@gmail.com>
2026-03-24 15:46:16 +0500

    project section updated + some other changes

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,43 +1,115 @@
 // import { project_layout } from '../assets/imges'
-import {  motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 import '../css/project.css'
 import { Link } from 'react-router-dom'
+
+const projects = [
+  {
+    id: '01',
+    name: 'PakStats – Pakistan Indicator Dashboard',
+    technologies: 'React, Vite, World Bank API',
+    description:
+      'Built an interactive data visualization website using React that showcases Pakistan’s key economic indicators such as poverty, trade, inflation, and education. Integrated World Bank Open Data API for live statistics, designed reusable components for charts, and implemented a clean, user-friendly interface to make complex data understandable for the general public.'
+  },
+  {
+    id: '02',
+    name: 'TodoSync - Task Management Web App',
+    technologies: 'React, Node.js, Express, MongoDB, and Mongoose.',
+    description:
+      'A full-stack todo management app with user authentication. Each user can securely add, view, edit, and delete their own todos. Integrated React Router for dynamic routing and Axios for client-server communication. Todos are filtered by user email and fetched from MongoDB via RESTful API endpoints. Currently source code is available on GitHub and will be deployed live soon.'
+  },
+  {
+    id: '03',
+    name: 'MediaHub – Multimedia Search & Download Platform',
+    technologies: 'React, Vite, Axios, Pexels API',
+    description: 'MediaHub is a modern frontend project where users can search, explore, and download high-quality images and videos. Built using React and Vite, this app integrates with the Unsplash and Pexels APIs to fetch media content dynamically. It features a responsive layout, hover controls on videos, and a clean 3-column grid for media display. A search feature allows users to filter results by keyword and type. Currently hosted on GitHub and optimized for future deployment on Vercel.'
+  },
+  { id: '04', name: 'Website Clones & UI Practice Projects', technologies: 'HTML, CSS, JavaScript', description: 'Created several frontend clones of websites as part of self-learning and UI development practice. These projects helped strengthen my skills in layout design, responsiveness, and understanding modern web structure. Though not yet uploaded to GitHub, they reflect hands-on experience with real-world UI patterns and component-based design.' }
+]
 export default function Project () {
   return (
     <>
       <div className='project__section'>
         {/* <img src={project_layout} alt="" /> */}
         <h2>My Projects</h2>
-        <motion.div initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 , delay: 0.2}}
-            viewport={{ once: true}} className='Porject__Card'>
-          <h3>01</h3>
+       { projects.map((project, index)=>(
+          <motion.div
+          key={index}
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          viewport={{ once: true }}
+          className='Porject__Card'
+        >
+          <h3>{project.id}</h3>
           <Link
-            target='_blank'
-            to='https://pak-stats.vercel.app/'
+            // target='_blank'
+            to=''
+            aria-disabled
           >
-            <div className='heading'>
-              <h2>PakStats – Pakistan Indicator Dashboard</h2>
-              <p>(React, Vite, World Bank API)</p>
+            <div className='content'>
+              <div className='heading'>
+                <h2>{project.name}</h2>
+                <p>({project.technologies})</p>
+              </div>
+              <div className='description'>
+                <p>
+                  {project.description}
+                </p>
+              </div>
             </div>
-            <div className='description'>
-              <p>
-                Built an interactive data visualization website using React that showcases Pakistan’s key economic indicators such as poverty, trade, inflation, and education. Integrated World Bank Open Data API for live statistics, designed reusable components for charts, and implemented a clean, user-friendly interface to make complex data understandable for the general public.
-              </p>
+            <div className='images'>
+             
             </div>
           </Link>
         </motion.div>
-        <motion.div initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 }}
-            viewport={{ once: true}} className='Porject__Card'>
-          <h3>02</h3>
+        ))}
+
+
+
+
+        {/* <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          viewport={{ once: true }}
+          className='Porject__Card'
+        >
+          <h3>01</h3>
           <Link
-            target='_blank'
-            to='https://todo-sync-jet.vercel.app/'
+            // target='_blank'
+            to='/project'
           >
+            <div className='content'>
+              <div className='heading'>
+                <h2>PakStats – Pakistan Indicator Dashboard</h2>
+                <p>(React, Vite, World Bank API)</p>
+              </div>
+              <div className='description'>
+                <p>
+                  Built an interactive data visualization website using React
+                  that showcases Pakistan’s key economic indicators such as
+                  poverty, trade, inflation, and education. Integrated World
+                  Bank Open Data API for live statistics, designed reusable
+                  components for charts, and implemented a clean, user-friendly
+                  interface to make complex data understandable for the general
+                  public.
+                </p>
+              </div>
+            </div>
+            <div className='images'></div>
+          </Link>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
+          className='Porject__Card'
+        >
+          <h3>02</h3>
+          <Link target='_blank' to='https://todo-sync-jet.vercel.app/'>
             <div className='heading'>
               <h2>TodoSync - Task Management Web App</h2>
               <p>(React, Node.js, Express, MongoDB, and Mongoose.)</p>
@@ -55,10 +127,13 @@ export default function Project () {
             </div>
           </Link>
         </motion.div>
-        <motion.div initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 , delay: 0.2}}
-            viewport={{ once: true}} className='Porject__Card'>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.2 }}
+          viewport={{ once: true }}
+          className='Porject__Card'
+        >
           <h3>03</h3>
           <Link
             target='_blank'
@@ -70,15 +145,24 @@ export default function Project () {
             </div>
             <div className='description'>
               <p>
-                MediaHub is a modern frontend project where users can search, explore, and download high-quality images and videos. Built using React and Vite, this app integrates with the Unsplash and Pexels APIs to fetch media content dynamically. It features a responsive layout, hover controls on videos, and a clean 3-column grid for media display. A search feature allows users to filter results by keyword and type. Currently hosted on GitHub and optimized for future deployment on Vercel.
+                MediaHub is a modern frontend project where users can search,  explore, and download high-quality images and videos. Built
+                using React and Vite, this app integrates with the Unsplash and
+                Pexels APIs to fetch media content dynamically. It features a
+                responsive layout, hover controls on videos, and a clean
+                3-column grid for media display. A search feature allows users
+                to filter results by keyword and type. Currently hosted on
+                GitHub and optimized for future deployment on Vercel.
               </p>
             </div>
           </Link>
         </motion.div>
-        <motion.div initial={{ opacity: 0, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 , delay: 0.4}}
-            viewport={{ once: true}} className='Porject__Card'>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.4 }}
+          viewport={{ once: true }}
+          className='Porject__Card'
+        >
           <h3>04</h3>
           <Link target='_blank' to='https://github.com/Huraira-Arshad-Abbasi'>
             <div className='heading'>
@@ -96,11 +180,19 @@ export default function Project () {
               </p>
             </div>
           </Link>
-        </motion.div>
-        <motion.div initial={{ opacity: 0 }}
-            whileInView={{ opacity: .5}}
-            transition={{ duration: 0.8, delay: 0.5}}
-            viewport={{ once: true }} className="upcoming__projects">
+        </motion.div> */}
+        
+
+
+
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 0.5 }}
+          transition={{ duration: 0.8, delay: 0.5 }}
+          viewport={{ once: true }}
+          className='upcoming__projects'
+        >
           <h2>More Projects Coming Soon...</h2>
         </motion.div>
       </div>
---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+import '../css/ProjectDetails.css'
+
+export default function ProjectDetails() {
+  return (
+    <div className='details_container'>
+      here is the details of the project
+    </div>
+  )
+}
