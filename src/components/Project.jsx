f9d650068ad7cab6e0f6b01be69d6f5b8989b881
Huraira Arshad <hurairaabbasi647@gmail.com>
2026-03-27 11:36:30 +0500

    images added+ update UI

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,18 @@
 // import { project_layout } from '../assets/imges'
+import { useState } from 'react'
 import { motion } from 'framer-motion'
+import { MHCover, MH1, MH2, MH3, MH4, MH5 } from '../assets/imges'
+import { PSCover, PS1, PS2, PS3, PS4, PS5, PS6, PS7 } from '../assets/imges'
+import { TodoCover, TS1, TS2 } from '../assets/imges'
+import ProjectDetails from './ProjectDetails'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import { Pagination } from 'swiper/modules'
+import 'swiper/css/pagination'
 
 import '../css/project.css'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const projects = [
   {
@@ -10,64 +20,96 @@ const projects = [
     name: 'PakStats – Pakistan Indicator Dashboard',
     technologies: 'React, Vite, World Bank API',
     description:
-      'Built an interactive data visualization website using React that showcases Pakistan’s key economic indicators such as poverty, trade, inflation, and education. Integrated World Bank Open Data API for live statistics, designed reusable components for charts, and implemented a clean, user-friendly interface to make complex data understandable for the general public.'
+      'Built an interactive data visualization website using React that showcases Pakistan’s key economic indicators such as poverty, trade, inflation, and education. Integrated World Bank Open Data API for live statistics, designed reusable components for charts, and implemented a clean, user-friendly interface to make complex data understandable for the general public.',
+    images: [PSCover, PS1, PS2, PS3, PS4, PS5, PS6, PS7]
   },
   {
     id: '02',
     name: 'TodoSync - Task Management Web App',
     technologies: 'React, Node.js, Express, MongoDB, and Mongoose.',
     description:
-      'A full-stack todo management app with user authentication. Each user can securely add, view, edit, and delete their own todos. Integrated React Router for dynamic routing and Axios for client-server communication. Todos are filtered by user email and fetched from MongoDB via RESTful API endpoints. Currently source code is available on GitHub and will be deployed live soon.'
+      'A full-stack todo management app with user authentication. Each user can securely add, view, edit, and delete their own todos. Integrated React Router for dynamic routing and Axios for client-server communication. Todos are filtered by user email and fetched from MongoDB via RESTful API endpoints. Currently source code is available on GitHub and will be deployed live soon.',
+    images: [TodoCover, TS1, TS2]
   },
   {
     id: '03',
     name: 'MediaHub – Multimedia Search & Download Platform',
     technologies: 'React, Vite, Axios, Pexels API',
-    description: 'MediaHub is a modern frontend project where users can search, explore, and download high-quality images and videos. Built using React and Vite, this app integrates with the Unsplash and Pexels APIs to fetch media content dynamically. It features a responsive layout, hover controls on videos, and a clean 3-column grid for media display. A search feature allows users to filter results by keyword and type. Currently hosted on GitHub and optimized for future deployment on Vercel.'
+    description:
+      'MediaHub is a modern frontend project where users can search, explore, and download high-quality images and videos. Built using React and Vite, this app integrates with the Unsplash and Pexels APIs to fetch media content dynamically. It features a responsive layout, hover controls on videos, and a clean 3-column grid for media display. A search feature allows users to filter results by keyword and type. Currently hosted on GitHub and optimized for future deployment on Vercel.',
+    images: [MHCover, MH1, MH2, MH3, MH4, MH5]
   },
-  { id: '04', name: 'Website Clones & UI Practice Projects', technologies: 'HTML, CSS, JavaScript', description: 'Created several frontend clones of websites as part of self-learning and UI development practice. These projects helped strengthen my skills in layout design, responsiveness, and understanding modern web structure. Though not yet uploaded to GitHub, they reflect hands-on experience with real-world UI patterns and component-based design.' }
+  {
+    id: '04',
+    name: 'Website Clones & UI Practice Projects',
+    technologies: 'HTML, CSS, JavaScript',
+    description:
+      'Created several frontend clones of websites as part of self-learning and UI development practice. These projects helped strengthen my skills in layout design, responsiveness, and understanding modern web structure. Though not yet uploaded to GitHub, they reflect hands-on experience with real-world UI patterns and component-based design.',
+    images: []
+  }
 ]
+
 export default function Project () {
+  const [expandFlag, setExpandFlag] = useState(false)
+  const [selectedProject, setSelectedProject] = useState(null)
   return (
     <>
       <div className='project__section'>
         {/* <img src={project_layout} alt="" /> */}
         <h2>My Projects</h2>
-       { projects.map((project, index)=>(
+        {projects.map((project, index) => (
           <motion.div
-          key={index}
-          initial={{ opacity: 0, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
-          viewport={{ once: true }}
-          className='Porject__Card'
-        >
-          <h3>{project.id}</h3>
-          <Link
-            // target='_blank'
-            to=''
-            aria-disabled
+            key={index}
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            viewport={{ once: true }}
+            className='Porject__Card'
           >
-            <div className='content'>
-              <div className='heading'>
-                <h2>{project.name}</h2>
-                <p>({project.technologies})</p>
+            <h3>{project.id}</h3>
+            <div className='card_container'>
+              <div className='content'>
+                <div className='heading'>
+                  <h2>{project.name}</h2>
+                  <p>({project.technologies})</p>
+                </div>
+                <div className='description'>
+                  <p>{project.description}</p>
+                  <button
+                    onClick={() => {
+                      setExpandFlag(!expandFlag), setSelectedProject(project)
+                    }}
+                  >
+                    see details
+                  </button>
+                </div>
               </div>
-              <div className='description'>
-                <p>
-                  {project.description}
-                </p>
+              <div className='images'>
+                <Swiper
+                  modules={[ Pagination]}
+                  pagination={{ clickable: true }}
+                  
+                  loop
+                >
+                  {project.images.map(img => (
+                    <SwiperSlide key={img}>
+                      <img src={img} />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
               </div>
             </div>
-            <div className='images'>
-             
-            </div>
-          </Link>
-        </motion.div>
+          </motion.div>
         ))}
 
-
-
+        {expandFlag && (
+          <div>
+            <ProjectDetails
+              setExpandFlag={setExpandFlag}
+              project={selectedProject}
+            />
+          </div>
+        )}
 
         {/* <motion.div
           initial={{ opacity: 0, y: 100 }}
@@ -181,10 +223,6 @@ export default function Project () {
             </div>
           </Link>
         </motion.div> */}
-        
-
-
-
 
         <motion.div
           initial={{ opacity: 0 }}
--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -1,10 +1,39 @@
 // import React from 'react'
-import '../css/ProjectDetails.css'
+import PropTypes from 'prop-types'
+import '../css/projectDetails.css'
+// import { cover } from '../assets/imges'
 
-export default function ProjectDetails() {
+export default function ProjectDetails ({ setExpandFlag, project }) {
   return (
     <div className='details_container'>
-      here is the details of the project
+      <div className='images'>
+        {project.images.map((image, index) => (
+          <img key={index} src={image} alt={`${project.name} screenshot ${index + 1}`} />
+        ))}
+      </div>
+      <div className='content'>
+        <div className='heading'>
+          <h2>{project.name}</h2>
+          <div className='links'>
+            {/* three links will button will be here */}
+            <button disabled>Readme.md</button>
+            <button >GitHub</button>
+            <button>Live Preview</button>
+          </div>
+        </div>
+        <div className='description'>
+          <p>{project.technologies}</p>
+          <p>{project.description}</p>
+        </div>
+      </div>
+
+      <button type='button' onClick={() => setExpandFlag(false)}>
+        X
+      </button>
     </div>
   )
+}
+ProjectDetails.propTypes = {
+  setExpandFlag: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired
 }
